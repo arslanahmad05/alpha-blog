fb267108282fa40f2560ac3b2578247c214ec448
arslanahmad05 <arslanahmad0075@gmail.com>
2020-01-23 20:37:51 +0500

    create User

--- app/models/course.rb
+++
@@ -1,4 +0,0 @@
-class Course < ActiveRecord::Base
-  validates :name, presence: true, length: {minimum: 5, maximum: 10}
-  validates :description, presence: true, length: {minimum: 5, maximum: 50}
-end
--- app/models/student.rb
+++
@@ -1,5 +0,0 @@
-class Student < ActiveRecord::Base
-  validates :name, presence: true, length: {minimum: 3, maximum: 10}
-  validates :email, presence: true, length: {minimum: 5, maximum: 20}
-
-end
