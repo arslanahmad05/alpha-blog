8aadfe4cd1294c32f7d62013c78c4e2b859de81d
arslanahmad05 <arslanahmad0075@gmail.com>
2020-01-24 11:31:59 +0500

    signup User

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
 
   root 'pages#home'
   resources :articles
+  get 'signup', to: 'users#new'
+  # post 'users', to: 'user#create'
+  resources :users, except: [:new]
 
 end
